1efa59780cdb178bba4ec63ffdd2e320427f78ad
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-05-19 16:58:02 +0900

    es5-es6 초안 작성, 프로젝트 설명 글 작성

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,14 +1,12 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: '모바일 청첩장 클론 코딩',
+    description: `요즘 모바일 청첩장이 대세 아니겠습니까. 자고로 시간 빌게이츠라면 얼마 하지않는(네이버 스마트스토어에서 9,900원) 모바일 청첩장도 스스로 구현할 수 있어야 하지 않을까요. 마침 React, Redux, tailwind와 vite를 공부하고 있는데 이 기술들을 바탕으로 모바일 청첩장을 SPA처럼 구현해보겠습니다. 제 디자인 실력이 워낙 허접이어서 디자인은 실제 사이트를 보면서 따라해보려고 합니다. 레츠고!`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://github.com/funkyblues/wedding-clone',
   },
   {
-    title: 'The Time Machine',
+    title: 'ChatGPT와 함께하는 주식 뉴스 분석',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
