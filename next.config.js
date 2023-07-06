4feb01d5eb488f4d4351b8a5d56dd66bf75362f4
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-07-06 15:49:44 +0900

    Refactor: next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const ContentSecurityPolicy = `
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self';
+  font-src 'self' https://fonts.gstatic.com;
   frame-src giscus.app
 `
 
